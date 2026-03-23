328122d245d9a80b65193162b6bed68f470d5350
Vaibhav Umbarkar <vaibhavumbarkar8485@gmail.com>
2026-03-23 12:31:32 +0530

    version 1.0

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+
+// ✅ FIX: new Socket.IO syntax
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+let usernames = [];
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server running...");
+});
+
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+// ✅ FIX: modern connection event
+io.on('connection', function(socket){
+
+  socket.on('new user', function(data, callback){
+    if(usernames.indexOf(data) !== -1){
+      callback(false);
+    } else {
+      callback(true);
+      socket.username = data;
+      usernames.push(socket.username);
+      updateUsernames();
+    }
+  });
+
+  function updateUsernames(){
+    io.emit('usernames', usernames); // ✅ simplified
+  }
+
+  socket.on('send message', function(data){
+    io.emit('new message', {
+      msg: data,
+      user: socket.username
+    });
+  });
+
+  socket.on('disconnect', function(){
+    if(!socket.username) return;
+
+    // ❗ FIX: splice bug
+    usernames.splice(usernames.indexOf(socket.username), 1);
+
+    updateUsernames();
+  });
+});
